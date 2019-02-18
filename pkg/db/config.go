7bf11628e5848205fb82526cca6819312c46d0f4
Jacob Kaufmann <jacobkaufmann18@gmail.com>
2019-02-17 20:45:07 -0800

    define constants and method to retrieve config from env

--- pkg/db/config.go
+++ pkg/db/config.go
@@ -1,10 +1,39 @@
 package db
 
+import (
+	"errors"
+	"os"
+)
+
+const (
+	HostKey     = "host"
+	PortKey     = "port"
+	UsernameKey = "username"
+	PasswordKey = "password"
+	DBKey       = "db"
+)
+
 // A Config holds configuration information about a datastore
 type Config struct {
 	Host         string
-	Port         int
+	Port         string
 	Username     string
 	Password     string
 	DatabaseName string
 }
+
+// ConfigFromEnv returns a Config built from the specified environment variables
+func ConfigFromEnv(varNames map[string]string) *Config {
+	cfg := &Config{}
+
+	cfg.Host = os.Getenv(varNames[HostKey])
+	cfg.Port = os.Getenv(varNames[PortKey])
+	cfg.Username = os.Getenv(varNames[UsernameKey])
+	cfg.Password = os.Getenv(varNames[PasswordKey])
+	cfg.DatabaseName = os.Getenv(varNames[DBKey])
+
+	return cfg
+}
+
+// ErrEnvVarNotFound is a failure to retrieve an environment variable
+var ErrEnvVarNotFound = errors.New("environment variable not found")
